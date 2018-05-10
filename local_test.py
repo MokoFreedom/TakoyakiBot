6632d5811e3ebfe65e0796ee05cd0bf08abd1722
MokoFreedom <momoichigodayo@gmail.com>
2018-05-10 22:24:56 +0900

    change access token

--- local_test.py
+++ local_test.py
@@ -1,6 +1,11 @@
+from apscheculer.schedulers.blocking import BlockingScheduler
 from takoyaki import Takoyaki
 
-takotako = Takoyaki(1) # tweet_typeをテストに設定。
-nya = takotako.nyan()
-print(nya)
-print(len(nya))
+sched = BlockingScheduler()
+
+@sched.scheduled_job("interbal", minutes=1)
+def tweet_test():
+    takotako = Takoyaki(1) # tweet_typeをテストに設定。
+    nya = takotako.nyan()
+    print(nya)
+    print(len(nya))
