13c003cad598a0f1ca85691c378ab2ec7561ae6f
MokoFreedom <momoichigodayo@gmail.com>
2018-05-10 21:19:39 +0900

    change clock to UTC

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ import regular_tweet
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job("interval", minutes=1, start_date="2018-05-10 21:10:00")
+@sched.scheduled_job("interval", minutes=1, start_date="2018-05-10 12:22:00")
 def timed_job():
     regular_tweet.moko_takoyaki()
 
