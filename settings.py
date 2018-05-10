9393cfead99eb0d7c070911e8b2d760d958ef922
MokoFreedom <momoichigodayo@gmail.com>
2018-05-10 14:56:35 +0900

    fix setting

--- settings.py
+++ settings.py
@@ -5,10 +5,10 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 
 
-dotenv_path = join(diename(__file__), ".env")
-loaddotenv(dotenv_path)
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
 
-CONSUMER_KEY = os.envision.get("CONSUMER_KEY")
+CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
 CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
