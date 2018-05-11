dda57e1adda8b57ea693abf11c40f47709ae95cd
MokoFreedom <momoichigodayo@gmail.com>
2018-05-12 07:39:40 +0900

    fix bug

--- clock.py
+++ clock.py
@@ -5,8 +5,8 @@ import regular_tweet
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job("interval", hours=4, start_date="2018-05-11 19:00:00")
-, listener=my_stream_listenerdef timed_job():
+@sched.scheduled_job("interval", hours=4, start_date="2018-05-11 23:00:00")
+def timed_job():
     regular_tweet.moko_takoyaki()
 
 if __name__ == "__main__":
