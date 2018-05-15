e5af2164578c2241ea624f8071acd93ede4b0b0c
MokoFreedom <momoichigodayo@gmail.com>
2018-05-15 23:20:28 +0900

    change start date

--- clock.py
+++ clock.py
@@ -6,7 +6,7 @@ import my_stream
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job("interval", hours=4, start_date="2018-05-19 15:00:00")     
+@sched.scheduled_job("interval", hours=4, start_date="2018-05-15 15:00:00")     
 def timed_job():
     regular_tweet.moko_takoyaki(1)
 
