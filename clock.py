2e354ea4b139bf2939296e5eb098cd660392b50c
MokoFreedom <momoichigodayo@gmail.com>
2018-05-24 22:12:09 +0900

    change schecduled_job to cron and add odaibako

--- clock.py
+++ clock.py
@@ -8,9 +8,9 @@ import my_stream
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job("interval", hours=4,  start_date="2018-05-23 11:00:00")     
+@sched.scheduled_job("cron", hours="3,7,11,15,19,23")     
 def timed_takoyaki_tweet():
-    regular_tweet.takoyaki_tweet(0)
+    regular_tweet.takoyaki_tweet(1)
 
 
 @sched.scheduled_job("cron", hour="0,12")
---
+++ test.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+
+import tweepy
+import regular_tweet
+from get_oauth import get_oauth
+from takoyaki import Takoyaki
+
+
+class TakoyakiTest:
+
+    def local(self):
+    
+        takotako = Takoyaki(1) # tweet_typeをテストに設定。
+        nya = takotako.nyan()
+        print(nya)
+        print(len(nya))
+
+
+    def not_local(self):
+
+        regular_tweet.takoyaki_tweet(1)
+
+
+class OdaibakoTest:
+
+    def local(self):
+        
+        pass
+
+
+    def not_local(self):
+
+        regular_tweet.odaibako_tweet()
+
+
+if __name__ == "__main__":
+
+    # テスト内容記述
+    tako = TakoyakiTest()
+    odai = OdaibakoTest()
+
+    #tako.local()
+    #tako.not_local()
+
+    #odai.local()
+    odai.not_local()
