dad8d2bfc5c742beb610df610b74530f89312ea3
kgtkr <kgtkr.jp@gmail.com>
2018-08-17 00:46:05 +0900

    1分に一回TL取得する仕様に

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ sched = BlockingScheduler()
 
 # 日本時間で4の倍数時に行うたこ焼きの定期ツイ
 # 設定のhourはUTC
-@sched.scheduled_job("cron", hour="3,7,11,15,19,23")     
+@sched.scheduled_job("cron", hour="3,7,11,15,19,23")
 def timed_takoyaki_tweet():
     regular_tweet.takoyaki_tweet(0)
 
@@ -19,13 +19,17 @@ def timed_takoyaki_tweet():
 # 日本時間で9時,21時に行うお題箱の定期ツイ
 # 設定のhourはUTC
 @sched.scheduled_job("cron", hour="0,12")
-def timed_odaibako_tweet() :
+def timed_odaibako_tweet():
     regular_tweet.odaibako_tweet()
 
 
-if __name__ == "__main__":
+# 1分に一回TL取得
+@sched.scheduled_job("cron", second="0")
+def tl_tweet():
+    print("TL取得")
+    streaming.tl_check()
 
-    # ストリーミング開始
-    streaming.takoyaki_streaming()
+
+if __name__ == "__main__":
     # スケジューラー開始
     sched.start()
--- streaming.py
+++ streaming.py
@@ -4,80 +4,48 @@ import tweepy
 import Library
 
 
-api = None
+def tweet(api, status):
+
+    # "たこ焼きガチャ"もしくは"たこやきガチャ"というワードをTLで発見
+    if "たこ焼きガチャ" in status.text or "たこやきガチャ" in status.text:
+        # RTは除外
+        if (not status.retweeted) and ("RT @" not in status.text):
+            # status.auther.screen_name でツイ主のユーザー名を取得
+            tweet_text = "@" + str(status.author.screen_name) + "\n"
+            # Takoyakiのtweet_typeをリプに
+            takotako = Library.takoyaki.Takoyaki(2)
+            tweet_text += takotako.nyan()
+
+            # ツイートがTwitterの文字数制限を超えていないかチェック
+            # 超えていたら縮小
+            tweet_text = Library.character_count.check_text_length(
+                tweet_text)
+
+            # logに残す
+            # 残さなくても大丈夫なのですが、私が残したかったので
+            print(tweet_text.split("\n")[0])  # @userID のところだけ表示
+
+            try:
+                # ツイートして、その送ったツイートの情報を受け取る
+                sent_tweet_status = api.update_status(
+                    tweet_text, status.id)
+            except:
+                sent_tweet_status = api.update_status(
+                    "エラーが発生しました...。", status.id)
+
+            # 今ツイートしたたこ焼きガチャのURLを自分自信(bot)にDMで送る
+            # これも行う必要は全く無いのですが、自分が記録として残しておきたかったので
+            dm_text = "https://twitter.com/{}/status/{}".format(str(sent_tweet_status.author.screen_name),
+                                                                sent_tweet_status.id_str)
+            api.send_direct_message(
+                screen_name="moko_takoyaki", text=dm_text)
 
 
-class TakoyakiListener(tweepy.StreamListener):
-
-    def __init__(self, api):
-        super().__init__(api)
-        self.me = self.api.me()
-
-
-    def on_status(self, status):
-
-        # "たこ焼きガチャ"もしくは"たこやきガチャ"というワードをTLで発見
-        if "たこ焼きガチャ" in status.text or "たこやきガチャ" in status.text:
-            # RTは除外
-            if (not status.retweeted) and ("RT @" not in status.text):
-                # status.auther.screen_name でツイ主のユーザー名を取得
-                tweet_text = "@" + str(status.author.screen_name)  + "\n"
-                # Takoyakiのtweet_typeをリプに
-                takotako = Library.takoyaki.Takoyaki(2)
-                tweet_text += takotako.nyan()
-
-                # ツイートがTwitterの文字数制限を超えていないかチェック
-                # 超えていたら縮小
-                tweet_text = Library.character_count.check_text_length(tweet_text)
-
-                # logに残す
-                # 残さなくても大丈夫なのですが、私が残したかったので
-                print(tweet_text.split("\n")[0]) # @userID のところだけ表示
-
-                try:
-                    # ツイートして、その送ったツイートの情報を受け取る
-                    sent_tweet_status = api.update_status(tweet_text, status.id)
-                except:
-                    sent_tweet_status = api.update_status("エラーが発生しました...。", status.id)
-
-                # 今ツイートしたたこ焼きガチャのURLを自分自信(bot)にDMで送る
-                # これも行う必要は全く無いのですが、自分が記録として残しておきたかったので
-                dm_text = "https://twitter.com/{}/status/{}".format(str(sent_tweet_status.author.screen_name),\
-                                                                        sent_tweet_status.id_str)
-                api.send_direct_message(screen_name="moko_takoyaki", text=dm_text)
-
-
-        return True
-
-
-    def on_event(self, event):
-        # 自動フォロー
-        if event.event == "follow":
-            if self.me.id != event.source["id"]:
-                source_user = event.source
-                event._api.create_friendship(source_user["id"])
-                print("followed by {} {}".format(source_user["name"], source_user["screen_name"]))
-
-
-    def on_error(self, status_code):
-
-        api.update_status("Omg...An error occured...Sorry...")
-        print("Omg...Got an error...: " + str(status_code))
-        return True
-
-
-    def on_timeout(self):
-
-        api.update_status("Timeout...nyan...Sorry...")
-        print("Timeout...nyan...")
-        return True
-
-
-def takoyaki_streaming():
-
+def tl_check():
     auth = Library.get_auth.get_auth()
-    global api
     api = tweepy.API(auth)
-
-    stream = tweepy.Stream(auth, TakoyakiListener(api), secure=True)
-    stream.userstream(async=True)
+    for status in api.home_timeline(count=200):
+        try:
+            tweet(api, status)
+        except tweepy.TweepError as e:
+            print(e.reason)
