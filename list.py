9e0fc17f20c4b17b05fd45e3352927707721ab4c
kgtkr <kgtkr.jp@gmail.com>
2018-08-17 13:48:53 +0900

    sync

--- list.py
+++ list.py
@@ -5,19 +5,19 @@ import typing
 list_name = "follow takoyaki"
 
 
-def list_members(api: tweepy.API, listID: str)->typing.Set[int]:
+def find_members(api: tweepy.API, listID: str)->typing.Set[int]:
     return set([x.id for x in tweepy.Cursor(api.list_members, list_id=listID).items()])
 
 
-def followers(api: tweepy.API)->typing.Set[int]:
+def find_followers(api: tweepy.API)->typing.Set[int]:
     return set(list(tweepy.Cursor(api.followers_ids, user_id=api.me().id).items()))
 
 
-def friends(api: tweepy.API)->typing.Set[int]:
+def find_friends(api: tweepy.API)->typing.Set[int]:
     return set(list(tweepy.Cursor(api.friends_ids, user_id=api.me().id).items()))
 
 
-def outgoing(api: tweepy.API)->typing.Set[int]:
+def find_outgoing(api: tweepy.API)->typing.Set[int]:
     return set([int(x) for x in tweepy.Cursor(api.friendships_outgoing).items()])
 
 
@@ -31,3 +31,31 @@ def find_list(api: tweepy.API)->int:
         if x.name == list_name:
             return x.id
     return create_list(api)
+
+
+def sync_list(api: tweepy.API, list_id: int, friends: typing.Set[int], members: typing.Set[int]):
+    # リストに追加
+    api.add_list_members(list_id=list_id, user_id=list(friends-members))
+    # リストから削除
+    api.remove_list_members(list_id=list_id, user_id=list(members-friends))
+
+
+def sync_friends(api: tweepy.API, friends: typing.Set[int], followers: typing.Set[int], outgoing: typing.Set[int]):
+    # フォロー
+    for id in followers-friends-outgoing:
+        api.create_friendship(user_id=id)
+
+    # リム
+    for id in friends-followers:
+        api.destroy_friendship(user_id=id)
+
+
+def sync(api: tweepy.API):
+    list_id = find_list(api)
+    members = find_members(api, list_id)
+    friends = find_friends(api)
+    followers = find_followers(api)
+    outgoing = find_outgoing(api)
+
+    sync_list(api, list_id, friends, members)
+    sync_friends(api, friends, followers, outgoing)
